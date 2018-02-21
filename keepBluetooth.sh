904f6f2f3c4466c645bd583029e6124b2a1d6be6
Felipe Pena <fcgp.21@gmail.com>
2018-02-21 14:29:30 -0300

    Update keepBluetooth.sh

--- keepBluetooth.sh
+++ keepBluetooth.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#Made by Felipe Pena
 
+echo "Bluetooth Connection Enforced!"
+echo "Press ctr+c to stop.."
 count=1
-while : 
+while :
 do
 afplay /System/Library/Sounds/Pop.aiff -v 0.0000.1
 ((count++))
